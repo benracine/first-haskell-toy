3e1fce80f110a8761c8db7c3c1c3b3056c847ceb
benracine <benracine@cornerstonenw.com>
2015-08-29 14:39:13 -0700

    .

--- count-states.hs
+++ count-states.hs
@@ -1,5 +1,6 @@
 import Data.List
 import Data.Text hiding (length, foldl, zip)
+import States
 
 main :: IO ()
 main = do
@@ -14,19 +15,8 @@ processString file_contents =
       memo ++ "\n" ++ fst pair ++ ", " ++ snd pair ++ " " ++ countOccurences pair 
     countOccurences pair = 
       show (length (subStrs file_contents (fst pair)) + length (subStrs file_contents (snd pair)))
-    states = ["Alabama", "Alaska", "Arizona", "Arkansas",
-      "California", "Colorado",    "Connecticut", "Delaware", "Florida", "Georgia",
-      "Hawaii", "Idaho",    "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky",
-      "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota",
-      "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada",   "New Hampshire",
-      "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota",
-      "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina",
-      "South Dakota",   "Tennessee", "Texas", "Utah", "Vermont", "Virginia",
-      "Washington", "West Virginia", "Wisconsin", "Wyoming"]
-    abbrs = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID",
-      "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO",
-      "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA",
-      "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
+    states = States.states
+    abbrs = States.abbrs
     states_and_abbrs = zip states abbrs
 
 subStrs :: String -> String -> [(Text, Text)]
